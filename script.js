7ccc9467b060ac8aa83f5017f3753241151a2ae3
MattxDamon <mbmattbarth@gmail.com>
2015-04-17 11:28:58 -0400

    firebase functionality added, everything seems to be working, just need to add styles

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var ref= new Firebase("https://anthillsignup.firebaseio.com/jobPost/exampleJob");
+
+
+var ref= new Firebase("https://anthillsignup.firebaseio.com/jobPost");
 
 var company_name;
 var job_category;
@@ -11,48 +13,46 @@ var pay_amount;
 var start_date;
 var end_date;
 
-//For initial if they don't change them
+
+$(document).ready(function() {
+	//For initial if they don't change them
 	company_name=$('#payType').val();
 
 
 
 
-$('#compName').change(function(){
-	company_name=this.val();
-})
-$('#jobCat').change(function(){
-	job_category=this.val();
-})
-$('#jobTitle').change(function(){
-	job_title=this.val();
-})
-$('#jobDesc').change(function(){
-	job_description=this.val();
-})
-$('#jobTime').change(function(){
-	job_timeframe=this.val();
-})
-$('#payYn').change(function(){
-	pay_yn=this.val();
-})
-$('#payType').change(function(){
-	pay_type=this.val();
-})
-$('#payAmount').change(function(){
-	pay_amount=this.val();
-})
-$('#startDate').change(function(){
-	start_date=this.val();
-})
-$('#endDate').change(function(){
-	end_date=this.val();
-})
+	$('#compName').change(function() {
+		company_name=$(this).val();
+	});
+	$('#jobCat').change(function() {
+		job_category=$(this).val();
+	});
+	$('#jobTitle').change(function() {
+		job_title=$(this).val();
+	});
+	$('#jobDesc').change(function() {
+		job_description=$(this).val();
+	});
+	$('#jobTime').change(function() {
+		job_timeframe=$(this).val();
+	});
+	$('#payYn').change(function() {
+		pay_yn=$(this).val();
+	});
+	$('#payType').change(function() {
+		pay_type=$(this).val();
+	});
+	$('#payAmount').change(function() {
+		pay_amount=$(this).val();
+	});
+	$('#startDate').change(function() {
+		start_date=$(this).val();
+	});
+	$('#endDate').change(function() {
+		end_date=$(this).val();
+	});
 
-$'#submit').click(function(){
-	sendData();
-})
-
-
+});
 
 function sendData(){
 	ref.push({
@@ -64,6 +64,7 @@ function sendData(){
 		paidYN: pay_yn,
 		payType: pay_type,
 		startDate: start_date,
+		endDate: end_date,
 		payAmount: pay_amount
   	});
 }
